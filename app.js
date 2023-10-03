23a58a5d1a243a010c35d8c95c682dc877f04f80
Braulio Chávez Martínez <balochavezmtz99@hotmail.com>
2023-10-03 17:07:54 -0600

    Changed from CommonJS to ESModules

--- app.js
+++ app.js
@@ -1,106 +1,21 @@
-const express = require('express')
-const crypto = require('node:crypto')
-const cors = require('cors')
+import express, { json } from 'express'
+import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
 
-const movies = require('./movies.json')
-const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+// EN EL FUTURO: el import del json será así:
+// import movies from './movies.json' with { type: 'json' }
+
+// Como leer un json en ESModules
+// import fs from 'node:fs'
+// const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
 
 const app = express()
-app.use(express.json())
-app.use(cors({
-  origin: (origin, callback) => {
-    const ACCEPTED_ORIGINS = [
-      'http://127.0.0.1:5500'
-    ]
-
-    if (ACCEPTED_ORIGINS.includes(origin)) {
-      return callback(null, true)
-    }
-
-    if (!origin) {
-      return callback(null, true)
-    }
-
-    return callback(new Error('Not allowed by CORS'))
-  }
-}))
+app.use(json())
+app.use(corsMiddleware())
 app.disable('x-powered-by')
 
 // Todos los recursos que sean MOVIES se identifica con /movies
-app.get('/movies', (req, res) => {
-  const { genre } = req.query
-  if (genre) {
-    const filteredMovies = movies.filter((movie) =>
-      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
-    )
-    return res.json(filteredMovies)
-  }
-  res.json(movies)
-})
-
-app.post('/movies', (req, res) => {
-  const result = validateMovie(req.body)
-
-  if (result.error) {
-    return res.sendStatus(422).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const newMovie = {
-    id: crypto.randomUUID(),
-    ...result.data
-  }
-
-  // Esto no sería REST, porque estamos guardando el estado de la aplicación en memoria
-  movies.push(newMovie)
-
-  res.sendStatus(201).json(newMovie)
-})
-
-app.get('/movies/:id', (req, res) => {
-  const { id } = req.params
-  const movie = movies.find((movie) => movie.id === id)
-  if (movie) return res.json(movie)
-
-  res.sendStatus(404).json({ message: 'Movie not found' })
-})
-
-app.delete('/movies/:id', (req, res) => {
-  const { id } = req.params
-
-  const movieIndex = movies.findIndex((movie) => movie.id === id)
-
-  if (movieIndex === -1) {
-    return res.sendStatus(404).json({ message: 'Movie not found' })
-  }
-
-  movies.splice(movieIndex, 1)
-
-  return res.json({ message: 'Movie deleted' })
-})
-
-app.patch('/movies/:id', (req, res) => {
-  const result = validatePartialMovie(req.body)
-
-  if (!result.success) {
-    return res.sendStatus(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const { id } = req.params
-  const movieIndex = movies.findIndex((movie) => movie.id === id)
-
-  if (movieIndex === -1) {
-    return res.sendStatus(404).json({ message: 'Movie not found' })
-  }
-
-  const updateMovie = {
-    ...movies[movieIndex],
-    ...result.data
-  }
-
-  movies[movieIndex] = updateMovie
-
-  return res.json(updateMovie)
-})
+app.use('/movies', moviesRouter)
 
 const PORT = process.env.PORT ?? 3000
 
